200701c5a2b1cbca8a4e9377721f1716b78c425b
ulises-morales <ulises.m.025@gmail.com>
2017-03-10 16:44:11 -0800

    Ejs with loops

---
+++ BackEnd/MEAN/Express/intermediate2/app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+
+var app = express();
+
+// root route
+app.get("/", function(req, res){
+  res.render("home.ejs");
+});
+
+// users route
+app.get("/user/:name", function(req, res){
+  var name = req.params.name;
+  res.render("welcome.ejs", {user_name: name});
+});
+
+
+// posts route
+app.get("/posts", function(req, res){
+  var posts = [
+    {title: "This is the first post", author: "Annonymous"},
+    {title: "This is the second post", author: "John"},
+    {title: "This is the third post", author: "Mike"},
+  ]
+  res.render("posts.ejs",{posts: posts});
+});
+
+
+// app listener
+app.listen(8000, function(){
+  console.log("Listening in port 8000");
+});
