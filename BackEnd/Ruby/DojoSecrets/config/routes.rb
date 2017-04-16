4e963c41120aa2246fb674e2899f96438645aa56
ulises-morales <ulises.m.025@gmail.com>
2017-04-15 19:07:17 -0700

    Secrets Dashboard

---
+++ BackEnd/Ruby/DojoSecrets/config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users
+  resources :secrets, only: [:index, :create, :destroy]
+  resources :likes, only: [:create, :destroy]
+  
+  # resources :users, only: [:new, :create, :show, :edit]
+
+  # get 'users/new' => 'users#new'
+
+  # get 'users/show' 
+
+  # get 'users/edit'
+
+  # get 'sessions/new'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
