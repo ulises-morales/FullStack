aa49add98df840d236ed5ccc7bd852832c07a4d6
ulises-morales <ulises.m.025@gmail.com>
2017-04-15 19:20:10 -0700

    Secrets dashboard

--- BackEnd/Ruby/Secrets/config/routes.rb
+++ BackEnd/Ruby/Secrets/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'sessions#new'
+
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :secrets, only: [:index, :create, :destroy]
