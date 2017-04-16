1dab1e5252fcdb30c89c58415c0fa9fe5b6e5aa1
ulises-morales <ulises.m.025@gmail.com>
2017-04-15 19:31:24 -0700

    Rails with partials

---
+++ BackEnd/Ruby/layouts/app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+    render layout: "two_column"
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to :back
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :favorite_language)
+    end
+end
